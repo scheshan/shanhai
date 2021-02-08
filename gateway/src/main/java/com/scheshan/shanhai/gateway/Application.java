5879121d771a6c0c3b13645df0b3ed753ab04412
heshan <5373827@qq.com>
2021-02-08 15:11:28 +0800

    Initialize gateway as a SpringBoot Webflux project

---
+++ gateway/src/main/java/com/scheshan/shanhai/gateway/Application.java
@@ -0,0 +1,18 @@
+package com.scheshan.shanhai.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ *
+ * @author heshan
+ * @date 2021/2/8
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
